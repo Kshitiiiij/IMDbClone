a1d8da4f8ad0ab53f7de2cba28ef8db1b7573770
kshitij <kshitij_987@protonmail.com>
2023-03-20 02:05:11 +0530

    created header section

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,21 +1,21 @@
 import MenuItem from "./MenuItem";
-import {AiFillHome} from 'react-icons/ai'
+import Link from "next/link";
+import { AiFillHome } from "react-icons/ai";
+import {BsFillInfoCircleFill} from 'react-icons/bs'
 export default function header() {
   return (
-    <div>
+    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
+      <div className="flex">
+        <MenuItem title="HOME" address="/" Icon={AiFillHome} />
 
-        <div className="">
-          <MenuItem
-           title='HOME'
-           address='/'
-           icon={AiFillHome}
-           />
+        <MenuItem title="ABOUT " address="/about" Icon={BsFillInfoCircleFill} />
+      </div>
 
-        </div>
-
-        <div className=""></div>
-
-
+      <div className="">
+        <Link href='/'>
+          <h2 className="2xl"><span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">IMDb</span><span className="text-xl hidden sm:inline">Clone</span></h2>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
