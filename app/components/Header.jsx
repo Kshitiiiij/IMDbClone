e31d8947cf31ff42fb5308def9a1584d0a3cab4d
kshitij <kshitij_987@protonmail.com>
2023-03-20 01:21:22 +0530

    passed props to menu item

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,6 +1,21 @@
+import MenuItem from "./MenuItem";
+import {AiFillHome} from 'react-icons/ai'
 export default function header() {
   return (
     <div>
+
+        <div className="">
+          <MenuItem
+           title='HOME'
+           address='/'
+           icon={AiFillHome}
+           />
+
+        </div>
+
+        <div className=""></div>
+
+
     </div>
   )
 }
