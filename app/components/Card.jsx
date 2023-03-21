7f1c0a88dd98d9c62edef7ddbf048e60b5424bd7
kshitij <kshitij_987@protonmail.com>
2023-03-21 19:37:20 +0530

    images

---
+++ app/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function Card({result}) {
+  return (
+    <div className='cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border-slate-400 transition-shadow duration-200 group'>
+      <Link href={`/movie/${result.id}`}>
+        <Image src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster.path}`} width={500} height={300}
+        className='sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200'
+        placeholder='blur'
+        blurDataURL='/spinner.svg'
+        ></Image>
+      </Link>
+    </div>
+  )
+}
--- app/components/Results.jsx
+++ app/components/Results.jsx
@@ -1,13 +1,12 @@
 import React from "react";
+import Card from "./Card";
 
 export default function Results({ results }) {
   console.log(results)
   return (
-    <div>
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-6xl mx-auto py-4" >
       {results.map((result) => (
-        <div className="" key={result.id}>
-          {result.title || result.name || result.original_title || result.orignal_name}
-        </div>
+        <Card key={result.id} result={result} />
       ))}
     </div>
   );
