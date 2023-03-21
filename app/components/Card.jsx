2ce25ecee287ce2ee2d9fa7775e6cb4c93f98f0f
kshitij <kshitij_987@protonmail.com>
2023-03-21 19:57:26 +0530

    added no cache

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { SpinnerInfinity } from 'spinners-react'
 
 export default function Card({result}) {
   return (
